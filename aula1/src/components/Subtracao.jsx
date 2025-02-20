a09545bbe0e55708e708a72adcb9e3b79d1fe1c5
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2025-02-19 21:12:14 -0300

    Adicionando coisas

---
+++ aula1/src/components/Subtracao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Subtracao = () => {
+  return (
+    <div>
+        O resultado de num3 + num4 é: {props.num3 + props.num4}
+    </div>
+  )
+}
+
+export default Subtracao
