c70182eb0b0511d7fd5a1c415391102af7252aed
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2025-03-12 14:12:32 -0300

    Mudança no app.jsx

--- aula1/src/App.jsx
+++ aula1/src/App.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import Adicao from './components/Adicao'
 import Subtracao from './components/Subtracao'
+import Multiplicacao from './components/Multiplicacao'
+import Divisao from './components/Divisao'
+import PrecisoEstudar from './components/PrecisoEstudar'
 
 const App = () => {
   return (
     <div>
-      <Adicao num1= {9} num2= {1}/>
+
+      <h1>Exercício 01 React</h1>
+
+      <hr />
+
+      <h2>Exercício 01</h2>
+
+      <br /> <br />
+
+      <Adicao num1 = {10} num2={11} />
+      <Subtracao num1 ={49} num2={9} />
+      <Multiplicacao num1={32} num2={6} />
+      <Divisao num1 ={30} num2={2} />
+
+      <br /> <br />
+
+      <hr />
+
+      <h2>Exercício 02</h2>
+
+      <br /><br />
+      <PrecisoEstudar nomeDaTecnologia="Vue3" />
     </div>
-  )
- 
-  return(
-  <div>
-  <Subtracao num3= {5} num4= {4}/>
-  </div>
   )
 }
 
