8270fb2e8644f5fee2c5a28b78fe9b8125090cd9
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2025-04-16 17:27:30 -0300

    CARA TA DANDO ALGUM ERRO

--- aula3/src/App.jsx
+++ aula3/src/App.jsx
@@ -6,18 +6,15 @@ const App = () => {
     <div>
       {
         [
-        {nome: "Joao", email:"joao@gmail.com", curso:"Sistemas para internet"},
-        {nome: "Maria", email:"maria@gmail.com", curso:"Sistemas de Informação"},
-        {nome: "Kellyane", email:"kellyane@gmail.com", curso:"Sistemas para internet"}
-        ].map((aluno) => 
-          <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso}/>
-        )
-      }
-
-    </div>
-
-      
-  )
-}
+        {nome: "Joao", email:"joao@gmail.com", curso:"Sistemas para internet", media:10},
+        {nome: "Maria", email:"maria@gmail.com", curso:"Sistemas de Informação", media:9.6},
+        {nome: "Kellyane", email:"kellyane@gmail.com", curso:"Sistemas para internet", media:7.0}
+        ].map((aluno, index) => 
+          <Aluno key={index} nome={aluno.nome} email={aluno.email} curso={aluno.curso}  media={aluno.media}
+          />
+        )}
+      </div>
+    );
+  };
 
 export default App
--- aula3/src/components/Aluno.jsx
+++ aula3/src/components/Aluno.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Aluno = ({nome, email, curso}) => {
+  const status = media >= 7 ? 'APROVADO' : 'REPROVADO';
+
   return (
     <div >
         <p> Nome: {nome}</p>
         <p> Email: {email}</p>
         <p> Curso: {curso}</p>
+        <p> Média: {media.toFixed(2)}</p>
+        <p>Status: {status}</p>
         <p> =================================== </p>
 
 
