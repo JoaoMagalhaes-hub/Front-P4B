a16e81a454123cf58fecb463c363c7719b64140a
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2025-04-30 20:53:21 -0300

    BRAINDAMAGE CACEEETA

--- aula6/src/pages/DpoLgpd.jsx
+++ aula6/src/pages/DpoLgpd.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const DpoLgpd = () => {
+  const [ dpoLgpd, setDpoLgpd] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/dpolgpd")//request
+    .then(response => setDpoLgpd(response.data))//resposta
+    .catch(error => console.error("Erro ao carregar a lista de DPO LGPD: ", error))
+  }, [])
+
+
   return (
     <div>
-       E eu seila 
+    <h1> NOTÍCIAS </h1>
+      <ul>
+        {
+          dpoLgpd.map((dpolgpd) => (
+          <li key={dpolgpd.id}>
+            <h2>{dpolgpd.titulo}</h2>
+            <h3>{dpolgpd.texto}</h3>
+          </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
