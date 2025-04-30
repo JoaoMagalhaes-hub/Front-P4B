9512a27eae464d2206fdecebae5a6eee7013bc88
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2025-04-30 20:35:32 -0300

    aaaaaaaaaaaah

--- aula6/src/pages/Noticias.jsx
+++ aula6/src/pages/Noticias.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Noticias = () => {
+
+  const [noticias, setNoticias] = useState ([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/noticias")//request
+    .then(response => setNoticias(response.data))//resposta
+    .catch(error => console.error("Erro ao carregar a lista de notícias: ", error))
+  }, [])
+
   return (
     <div>
 
-       <h2> E então vem noticiaaa</h2>
-      
+       <h1> NOTÍCIAS </h1>
+      <ul>
+        {
+          noticias.map((noticia) => (
+          <li key={noticia.id}>
+            <h2>{noticia.titulo}</h2>
+          </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
